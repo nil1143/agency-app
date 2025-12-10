0fb7e27496003d32197a0af536b908abd862ab39
Tom <tnil1143@gmail.com>
2025-12-10 19:16:43 +0000

    feat(components): add ServiceCard, Services, and Title components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import TrustedBy from "./components/TrustedBy";
+import Services from "./components/Services";
 
 const App = () => {
   const [theme, setTheme] = useState(
@@ -13,6 +14,7 @@ const App = () => {
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero />
       <TrustedBy />
+      <Services />
     </div>
   );
 };
