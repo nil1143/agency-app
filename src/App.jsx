1bb913ce0cc5b79165be8174049174d26137c740
Tom <tnil1143@gmail.com>
2025-12-11 00:27:48 +0000

    feat(components): add OurWork and Teams components

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import TrustedBy from "./components/TrustedBy";
 import Services from "./components/Services";
+import OurWork from "./components/OurWork";
+import Teams from "./components/Teams";
 
 const App = () => {
   const [theme, setTheme] = useState(
@@ -15,6 +17,8 @@ const App = () => {
       <Hero />
       <TrustedBy />
       <Services />
+      <OurWork />
+      <Teams />
     </div>
   );
 };
